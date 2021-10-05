bb252ddc526a52b2aec7ca24877a11651739132c
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-10-05 18:23:44 +0530

    Updated text

--- utills/constant.js
+++ utills/constant.js
@@ -13,4 +13,8 @@ export const STATIC_TEXT = {
   font_bold1: "500",
   font_bold2: "600",
   font_bold3: "700",
+  micka_has_brushed: "Micka has brushed",
+  micka_didnot_finish: "Micka did not finish",
+  arch_at: "arch at",
+  micka_finished_brushing: "Mica has finished brushing",
 };
