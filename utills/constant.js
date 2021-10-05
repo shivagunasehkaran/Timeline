7f6602960e17be0e97079dafc265cd67f4657a51
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-10-05 16:21:21 +0530

    Add constant file under utills

---
+++ utills/constant.js
@@ -0,0 +1,8 @@
+// applicaion text constants
+export const STATIC_TEXT = {
+  font_family: "Avenir",
+  font_family2: "Avenir Next",
+  font_bold1: "500",
+  font_bold2: "600",
+  font_bold3: "700",
+};
