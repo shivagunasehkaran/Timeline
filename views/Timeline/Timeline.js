4b516cef3553255091275ea4fcec7401734ce232
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-10-05 18:24:36 +0530

    Completed timeline calculations

--- views/Timeline/Timeline.js
+++ views/Timeline/Timeline.js
@@ -1,20 +1,139 @@
-import React from "react";
-import { FlatList, View, SafeAreaView } from "react-native";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, View } from "react-native";
 import BrushingDetails from "../../components";
+import { STATIC_TEXT, TIMELINE_STATUS } from "../../utills/Constant";
+import { SortedArray } from "../../utills/DateUtil";
 import { styles } from "./Timeline.style";
 
-const DATA = [
-  {
-    name: "Shiva",
-    age: 10,
-  },
-  {
-    name: "Gautham",
-    age: 11,
-  },
-];
-
 const Timeline = () => {
+  const [timeLineValue, setTimeLineValue] = useState([]);
+  let initialValue = 0;
+
+  useEffect(() => {
+    // initial call when page loads
+    renderTimeLine();
+  }, []);
+
+  // append values for time line status
+  const getTimelineStatus = (
+    brushingObj,
+    brushingDate,
+    brushingTime,
+    finalArray
+  ) => {
+    let obj = {};
+    obj.id = initialValue;
+    obj.brushingDate = brushingDate;
+    obj.brushingTime = brushingTime;
+    obj.arch = brushingObj.arch;
+    obj.status = brushingObj.interrupted
+      ? TIMELINE_STATUS.INCOMPLETE
+      : brushingObj.arch;
+    obj.interrupted = brushingObj.interrupted;
+    obj.message = !brushingObj.interrupted
+      ? `${STATIC_TEXT.micka_has_brushed} ${brushingObj.arch} ${STATIC_TEXT.arch_at} ${brushingDate} ${brushingTime}`
+      : `${STATIC_TEXT.micka_didnot_finish} ${brushingObj.arch} ${STATIC_TEXT.arch_at} ${brushingDate} ${brushingTime}`;
+    // push values into array
+    finalArray.push(obj);
+    initialValue++;
+  };
+
+  // calculate the time line mechanism based on json
+  const getTimelineStatusBasedOnTimeFrame = (
+    brushingObj,
+    brushingDate,
+    brushingTime,
+    finalArray
+  ) => {
+    let startTime = moment(
+      `${brushingDate} ${brushingTime}`,
+      "DD-MM-YYYY hh:mm:ss"
+    );
+    let endTime = moment(
+      `${finalArray[initialValue - 1].brushingDate} ${
+        finalArray[initialValue - 1].brushingTime
+      }`,
+      "DD-MM-YYYY hh:mm:ss"
+    );
+
+    let minutesDiff = startTime.diff(endTime, "minutes");
+    // check 30 mins time frame and set status
+    if (
+      minutesDiff < 30 &&
+      finalArray[initialValue - 1].arch !== brushingObj.arch &&
+      !brushingObj.interrupted &&
+      finalArray[initialValue - 1].status !== TIMELINE_STATUS.INCOMPLETE
+    ) {
+      finalArray[
+        initialValue - 1
+      ].message = `${STATIC_TEXT.micka_finished_brushing}`;
+      finalArray[initialValue - 1].status = TIMELINE_STATUS.COMPLETED;
+    } else if (
+      minutesDiff < 30 &&
+      (finalArray[initialValue - 1].arch !== brushingObj.arch ||
+        finalArray[initialValue - 1].arch === brushingObj.arch) &&
+      !brushingObj.interrupted &&
+      finalArray[initialValue - 1].status === TIMELINE_STATUS.INCOMPLETE
+    ) {
+      finalArray[
+        initialValue - 1
+      ].message = `${STATIC_TEXT.micka_has_brushed} ${brushingObj.arch} ${STATIC_TEXT.arch_at}  ${brushingDate} ${brushingTime}`;
+      finalArray[initialValue - 1].status =
+        brushingObj.arch === "upper"
+          ? TIMELINE_STATUS.UPPER_ARCH
+          : TIMELINE_STATUS.LOWER_ARCH;
+      finalArray[initialValue - 1].arch = brushingObj.arch;
+      finalArray[initialValue - 1].brushingDate = brushingDate;
+      finalArray[initialValue - 1].brushingTime = brushingTime;
+    } else if (minutesDiff > 30) {
+      // construt status for time line
+      getTimelineStatus(brushingObj, brushingDate, brushingTime, finalArray);
+    } else {
+      console.log("the final array in else part", finalArray);
+    }
+  };
+
+  // render time line status
+  const renderTimeLine = () => {
+    let finalArray = [];
+
+    // looping sorted array for getting status
+    for (let i = 0; i < SortedArray.length; i++) {
+      let brushingObj = SortedArray[i];
+      let date = brushingObj.date;
+      let brushingDate = moment.utc(date).format("D-MM-YYYY");
+      let brushingTime = moment.utc(date).format("hh:mm:ss");
+
+      // initial check
+      if (i === 0) {
+        // construt status for time line
+        getTimelineStatus(brushingObj, brushingDate, brushingTime, finalArray);
+      } else {
+        if (
+          Date(brushingDate) === Date(finalArray[initialValue - 1].brushingDate)
+        ) {
+          getTimelineStatusBasedOnTimeFrame(
+            brushingObj,
+            brushingDate,
+            brushingTime,
+            finalArray
+          );
+        } else {
+          // construt status for time line
+          getTimelineStatus(
+            brushingObj,
+            brushingDate,
+            brushingTime,
+            finalArray
+          );
+        }
+      }
+    }
+    console.log("the final array is", finalArray);
+    setTimeLineValue(finalArray);
+  };
+
   // child render item
   const childListRenderItem = ({ item, index }) => (
     <BrushingDetails item={item} />
@@ -28,7 +147,8 @@ const Timeline = () => {
       <SafeAreaView>
         <View style={styles.flatListView}>
           <FlatList
-            data={DATA}
+            data={timeLineValue}
+            showsVerticalScrollIndicator={false}
             renderItem={childListRenderItem}
             keyExtractor={childListKeyExtractor}
           />
