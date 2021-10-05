6e5175e44fdcc0211ae856ec570230121269d6f7
Shiva Gunasehkaran <danielshiva@gmail.com>
2021-10-05 16:12:02 +0530

    Create sample UI with basic folder structure

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Timeline from "./views/Timeline";
+
+const App = () => {
+  return <Timeline />;
+};
+
+export default App;
---
+++ views/Timeline/Timeline.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Timeline = () => {
+  return (
+    <View>
+      <Text>{"Hello Shiva"}</Text>
+    </View>
+  );
+};
+
+export default Timeline;
